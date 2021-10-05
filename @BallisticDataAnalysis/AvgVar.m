a6cf86de4327281996de76e37e9ac05c6a08a8e3
sadafmuhtasim <91855137+sadafmuhtasim@users.noreply.github.com>
2021-10-04 21:32:14 -0400

    Update AvgVar.m

--- @BallisticDataAnalysis/AvgVar.m
+++ @BallisticDataAnalysis/AvgVar.m
@@ -12,6 +12,48 @@ function success = AvgVar(obj)
     try
         disp(obj.SillyMessage);
         success = true;
+        obj = Analysis;
+        time_data = obj.A(:,1);
+        xdata = obj.A(:,2:3:31);
+        ydata = obj.A(:,3:3:31);
+        zdata = obj.A(:,4:3:31);
+        meanx = mean(xdata,2);
+        meany = mean(ydata,2);
+        meanz = mean(zdata,2);
+        varx = var(xdata,0,2);
+        vary = var(ydata,0,2);
+        varz = var(zdata,0,2);
+        figure(1)
+        subplot(2,2,1)
+        plot(time_data,meanx, 'linewidth', 2)
+        xlabel('t')
+        ylabel('x')
+        
+        subplot(2,2,2)
+        plot(time_data,meany, 'linewidth', 2)
+        xlabel('t')
+        ylabel('y')
+        
+        subplot(2,2,3)
+        plot(time_data,meanz, 'linewidth', 2)
+        xlabel('t')
+        ylabel('z')
+
+        figure(2)
+        subplot(2,2,1)
+        plot(time_data,varx, 'linewidth', 2)
+        xlabel('t')
+        ylabel('x')
+        
+        subplot(2,2,2)
+        plot(time_data,vary, 'linewidth', 2)
+        xlabel('t')
+        ylabel('y')
+        
+        subplot(2,2,3)
+        plot(time_data,varz, 'linewidth', 2)
+        xlabel('t')
+        ylabel('z')
     catch
         warning('Something went wrong in running AvgVar!');
         success = false;
